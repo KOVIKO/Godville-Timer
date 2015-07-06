d8f645465ae593bbe4cf7764b3b6146f99c584f7
Koviko <volectricity@gmail.com>
2015-07-06 11:47:15 -0400

    Made a tweak to prevent the timers showing too soon for users without enough collected data.

--- godville-timer.user.js
+++ godville-timer.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         Godville Timer
-// @version      1.1
+// @version      1.2
 // @description  Helps determine where the minute mark changes in Godville and send your godvoice right before it.
 // @author       Koviko <koviko.net@gmail.com>
 // @website      http://koviko.net/
@@ -110,6 +110,7 @@
 				latestSecond = null,
 				beforeSecond = null,
 				difference = null,
+				normalizedDifference = null,
 				beforeDifference = null,
 				exactSecond = null,
 				state = null;
@@ -120,11 +121,12 @@
 				latestSecond = toSeconds(allSeconds[allSeconds.length - 1].second);
 				beforeSecond = toSeconds(latestSecond - beforeThreshold);
 				difference = earliestSecond - latestSecond;
+				normalizedDifference = toSeconds(difference);
 				beforeDifference = latestSecond - beforeSecond;
 				exactSecond = earliestSecond + exactOffset;
 
 				// Determine if the current second is in between the latest and earliest seconds
-				if (toSeconds(difference) <= maxDisplayDifference) {
+				if (normalizedDifference <= maxDisplayDifference && normalizedDifference < 0) {
 					// Update the ready state
 					if (!isReady) {
 						isReady = true;
