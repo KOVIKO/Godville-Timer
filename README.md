b3b9e0fa628e136866daa44000b04826e7b6ce4b
Koviko <volectricity@gmail.com>
2015-10-15 12:14:50 -0400

    Completely changed server time estimation to use the official channel that Godville uses at akamai.com.

--- README.md
+++ README.md
@@ -12,14 +12,12 @@ To install the script, click the `godville-timer.user.js` in GitHub and then cli
 
 ##The UI
 
-![Godville Timer Bar](http://i.imgur.com/5P0ZXcz.png)
+![Godville Timer Bar](http://i.imgur.com/3v0MLl4.png)
 
 The Godville Timer bar will not block you from clicking anything beneath it. The only part of it that can be interacted with is the button in the queue box. The bar will appear all the way at the bottom of your window as to potentially cover up the least amount of space it possibly can. The bar's elements are as follows:
 
 * **Queue Box**: On the left is the element that allows you to queue your godvoice. If you have input a godvoice in the regular godvoice box (the input box under your godpower bar), then this will activate the <kbd>Send</kbd> button at the time which the script has deemed most appropriate.
-* **Latest Second**: The leftmost of the three timers represents the latest second found within a minute. This aims to be as close to the `:59` second-mark as possible.
-* **Main Timer**: The center of the three timers is the current second of your local time (i.e. the time on your computer's clock). This will change colors depending on the current time in relation to the **Latest Second** and **Earliest Second**.
-* **Earliest Second**: The rightmost of the three timers represents the earliest second found within a minute. This aims to be as close to the `:00` second-mark as possible.
+* **Timer**: On the right is the element that shows you the estimated server time. It changes colors as it approaches the `:59` second-mark and indicates at which point a queued godvoice will be activated.
 
 ##Why you should use it
 
@@ -27,19 +25,17 @@ There's a superstition amongst Godville players that if you use your godvoice ri
 
 The goal of this script is to make it so that your godvoice triggers at the `:59` second-mark and Godville responds at the `:00` second-mark. If successful then, visually in your diary, the command and the response should have different timestamps.
 
-While you could attempt to determine the offset manually, I used to do this and found that the offset seems to change from time to time. Whether this is due to lag or something else, I found that the offset might be 10 seconds in the morning, but 5 seconds by the evening and 15 seconds the next day. This script will attempt to adapt when that time might be changing.
+The current server time is visible by clicking the title of the "Remote Control" element. However, this display seems to hide eventually on its own. This script makes the current second always visible and updates every give minutes, just in case there is lag or your computer's time changes automatically.
 
-While you could send your godvoice manually, you may sometimes find yourself losing focus while doing something else and just barely missing the mark. This script will allow you to queue your next godvoice command and it will trigger itself right before its currently calculated minute-mark, provided you are not in combat. Note that this prediction is only as good as the data, so I recommend only using this feature when you have a window of less than 5 seconds in the timer for best results.
+While you could send your godvoice manually, you may sometimes find yourself losing focus while doing something else and just barely missing the mark. This script will allow you to queue your next godvoice command and it will trigger itself right before its currently calculated minute-mark, provided you are not in combat.
 
 ##How it works
 
-Whenever a journal entry is added, this script will take note of the time that the entry claims to have been added and compare it to the time on your computer. It will add this offset to the full data set, which is then used to determine the latest (`:59` second-mark) and earliest (`:00` second-mark) offsets within a given minute. These are represented by the two values surrounding the **Main Timer**. As long as the time between these two values is short enough (10 seconds), the **Main Timer** will pulse green while between the values. A few seconds before pulsing green (3 seconds before), the **Main Timer** will pulse yellow.
+This script retrieves the server time from the same source as Godville and uses this to determine the current second in relation to your local time. Whenever the `:59` second-mark is close, the **Timer** will pulse green. A few seconds before pulsing green (3 seconds before), the **Timer** will pulse yellow.
 
-The goal is to send a godvoice in the second prior to reaching the value to the right of the **Main Timer**. The script provides queuing functionality that will trigger your godvoice by pressing <kbd>Send</kbd> at that moment, provided you have text entered into the godvoice box and your hero is not currently in combat. Just click <kbd>Queue Voice</kbd> in the **Queue Box** to prepare the script to send your godvoice at the `:59` second-mark. If you change your mind about sending any godvoice commands, you can dequeue a queued godvoice at any time by clicking <kbd>Dequeue Voice</kbd> in the **Queue Box**. You are also free to change the text of your godvoice at any time without interrupting the queue.
+The goal is to send a godvoice in the second prior to reaching the value to the right of the **Timer**. The script provides queuing functionality that will trigger your godvoice by pressing <kbd>Send</kbd> at that moment, provided you have text entered into the godvoice box and your hero is not currently in combat. Just click <kbd>Queue Voice</kbd> in the **Queue Box** to prepare the script to send your godvoice at the `:59` second-mark. If you change your mind about sending any godvoice commands, you can dequeue a queued godvoice at any time by clicking <kbd>Dequeue Voice</kbd> in the **Queue Box**. You are also free to change the text of your godvoice at any time without interrupting the queue.
 
 ##Limitations
-
-This script will save data between sessions, but it will not transfer automatically between different browsers or computers. That means that if you play Godville from multiple browsers or locations, you will need to run this on all of them. This is incidentally a good thing when dealing with different computers as they might have different latency and a different local time. But overall, it's just because this script is entirely local and does not communicate over the Internet at all.
 
 The Queue will not send your godvoice during combat. This means that if you want to use combat godvoice commands (eg. hit), you'll need to send these manually. The large majority of godvoice commands do not work in combat, so the convenience of this feature outweighs the restrictions.
 
--- godville-timer.user.js
+++ godville-timer.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         Godville Timer
-// @version      1.4.2
+// @version      2.0.0
 // @description  Helps determine where the minute mark changes in Godville and send your godvoice right before it.
 // @author       Koviko <koviko.net@gmail.com>
 // @website      http://koviko.net/
@@ -11,14 +11,11 @@
 (function ($) {
 	"use strict";
 
-	var lastTimeSelector = "#diary .d_time:first",
-		containerTemplate = "<div id=godville-timer><div id=latest-second>:<span>--</span></div><div id=current-second>:<span></span></div><div id=earliest-second>:<span>--</span></div></div>",
+	var containerTemplate = "<div id=godville-timer><div id=current-second>:<span></span></div></div>",
 		queueTemplate = "<div class=queue><div class=not-queued><span>Not Queued</span><button id=queue-voice class=not-queued>Queue Voice</button></div><div class=queued><span>Queued</span><button id=dequeue-voice class=queued>Dequeue Voice</button></div></div>",
 		godvoiceButtonSelector = "#voice_submit_wrap input",
 		queueButtonSelector = "#queue-voice",
 		dequeueButtonSelector = "#dequeue-voice",
-		earliestSecondSelector = "#earliest-second span",
-		latestSecondSelector = "#latest-second span",
 		currentSecondContainerSelector = "#current-second",
 		currentSecondSelector = "#current-second span",
 		inCombatSelector = "#news .l_capt",
@@ -26,55 +23,30 @@
 		timeStates = { before: "before", during: "during", exact: "exact", after: "" },
 		defaultValue = "--",
 		queueClassName = "active",
-		readyClassName = "is-ready",
-		storageKey = "godville-timer",
-		timeDelimiter = ":",
-		halfDay = 12 * 60 * 60 * 1000,
-		maxTimeDifference = 59 * 1000,
-		secondsPerMinute = 60,
-		millisecondsPerMinute = secondsPerMinute * 1000,
-		maxLength = secondsPerMinute,
-		maxDisplayDifference = 10,
-		beforeThreshold = 3,
-		exactOffset = -1,
-		pad = function (value) { var abs = Math.abs(value); return (value < 0 ? "-" : "") + (abs < 10 ? "0" : "") + abs; },
-		toSeconds = function (value) { value = value % secondsPerMinute; return value < 0 ? value + secondsPerMinute : value; };
+    timeUrl = 'http://time.akamai.com/?iso',
+		maxTimeDifference = 5 * 59 * 1000,
+		pad = function (value) { var abs = Math.abs(value); return (value < 0 ? "-" : "") + (abs < 10 ? "0" : "") + abs; };
 
 	$(function () {
-		var lastTime = null,
-			lastUpdate = null,
-			allSeconds = JSON.parse(localStorage.getItem(storageKey)) || [],
-			container = $(containerTemplate).prependTo(document.body),
+		var container = $(containerTemplate).prependTo(document.body),
 			queue = $(queueTemplate).appendTo(container),
 			queueButton = $(queueButtonSelector, queue),
 			dequeueButton = $(dequeueButtonSelector, queue),
 			godvoiceButton = null,
-			earliestSecondElement = $(earliestSecondSelector, container),
-			latestSecondElement = $(latestSecondSelector, container),
 			currentSecondContainer = $(currentSecondContainerSelector, container),
 			currentSecondElement = $(currentSecondSelector, container),
-			currentSecond = (new Date()).getSeconds(),
+			currentSecond = null,
 			currentState = null,
-			isFirst = true,
+      lastUpdate = null,
+      timeAtUpdate = null,
+      isUpdating = false,
 			isQueued = false,
-			isReady = false,
-			sortTime,
-			onlyUnique,
 			isInCombat,
 			updateQueue,
 			changeState,
 			updateDisplay,
-			validateTime,
-			addTime,
+      updateTime,
 			checkTime;
-
-		sortTime = function (a, b) { // Sort the time elements
-			return a.second - b.second;
-		};
-
-		onlyUnique = function (value, index, self) { // Filter non-unique values
-			return self.indexOf(value) === index;
-		};
 
 		isInCombat = function () { // Determine whether the hero is in combat
 			return $(inCombatSelector).is(":visible");
@@ -112,175 +84,66 @@
 		};
 
 		updateDisplay = function () { // Update the display
-			var earliestSecond = null,
-				latestSecond = null,
-				beforeSecond = null,
-				difference = null,
-				normalizedDifference = null,
-				beforeDifference = null,
-				exactSecond = null,
-				state = null;
+      if (currentSecond === null) {
+        currentSecondElement.text(defaultValue);
+      } else {
+        var state = timeStates.after;
 
-			if (allSeconds.length > 1) {
-				// Determine the earliest and latest seconds
-				earliestSecond = toSeconds(allSeconds[0].second);
-				latestSecond = toSeconds(allSeconds[allSeconds.length - 1].second);
-				beforeSecond = toSeconds(latestSecond - beforeThreshold);
-				difference = earliestSecond - latestSecond;
-				normalizedDifference = toSeconds(difference);
-				beforeDifference = latestSecond - beforeSecond;
-				exactSecond = toSeconds(earliestSecond + exactOffset);
+        // Update the second display
+        if (currentSecond >= 59) {
+          state = timeStates.exact;
+        } else if (currentSecond >= 56) {
+          state = timeStates.during;
+        } else if (currentSecond >= 53) {
+          state = timeStates.before;
+        }
 
-				// Determine if the current second is in between the latest and earliest seconds
-				if (normalizedDifference <= maxDisplayDifference && normalizedDifference > 0) {
-					// Update the ready state
-					if (!isReady) {
-						isReady = true;
-						container.addClass(readyClassName);
-					}
+        // Update the current second display
+        currentSecondElement.text(pad(currentSecond));
 
-					// Update the second display
-					if (currentSecond === exactSecond) {
-						state = timeStates.exact;
-					} else if (difference >= 0 && (currentSecond < earliestSecond && currentSecond >= latestSecond)) {
-						state = timeStates.during;
-					} else if (difference < 0 && (currentSecond < earliestSecond || currentSecond >= latestSecond)) {
-						state = timeStates.during;
-					} else if (beforeDifference >= 0 && (currentSecond < latestSecond && currentSecond >= beforeSecond)) {
-						state = timeStates.before;
-					} else if (beforeDifference < 0 && (currentSecond < latestSecond || currentSecond >= beforeSecond)) {
-						state = timeStates.before;
-					} else {
-						state = timeStates.after;
-					}
-				} else if (isReady) {
-					isReady = false;
-					container.removeClass(readyClassName);
-				}
-
-				// Update the earliest & latest seconds
-				earliestSecondElement.text(pad(earliestSecond));
-				latestSecondElement.text(pad(latestSecond));
-			} else {
-				// Clear the earliest and latest seconds
-				earliestSecondElement.text(defaultValue);
-				latestSecondElement.text(defaultValue);
-			}
-
-			// Update the current second display
-			currentSecondElement.text(pad(currentSecond));
-
-			// Update the display's state
-			changeState(state);
+        // Update the display's state
+        changeState(state);
+      }
 		};
 
-		validateTime = function (timeObject) { // Add a new time and validate existing times against it
-			// Add new time object
-			if (timeObject) {
-				allSeconds.push(timeObject);
-			} else if (!allSeconds.length) {
-				return;
-			}
+    updateTime = function () { // Update the current time
+      if (!isUpdating) {
+        var start = Date.now();
+        isUpdating = true;
 
-			// Sort all times
-			allSeconds.sort(sortTime);
+        $.ajax(timeUrl, {
+          success: function (time) {
+            if (time) {
+              isUpdating = false;
+              lastUpdate = Date.now();
 
-			// Determine if the new time invalidates the earliest second
-			while ((allSeconds[0].second + secondsPerMinute) <= timeObject.second) {
-				allSeconds.shift();
-			}
-
-			// Determine if the new time invalidates the latest second
-			while ((allSeconds[allSeconds.length - 1].second - secondsPerMinute) >= timeObject.second) {
-				allSeconds.pop();
-			}
-
-			// Validate earliest and latest seconds are within 60 seconds of each other, just in case the data is corrupted
-			while ((allSeconds[allSeconds.length - 1].second - allSeconds[0].second) >= secondsPerMinute) {
-				// Remove first and last element
-				allSeconds.pop();
-				allSeconds.shift();
-			}
-
-			// Shrink the array when it gets too large
-			if (allSeconds.length > maxLength) {
-				allSeconds = allSeconds.filter(onlyUnique);
-			}
-
-			// Save to local storage
-			localStorage.setItem(storageKey, JSON.stringify(allSeconds));
-		};
-
-		addTime = function (seconds, time) { // Add a new time
-			if (isFirst) {
-				isFirst = false;
-				return;
-			}
-
-			// Add to collection and validate existing elements
-			validateTime({ second: seconds, time: time });
-		};
+              // Account for any lag in the request
+              timeAtUpdate = (new Date(time)).getTime() + (lastUpdate - start);
+            }
+          }
+        });
+      }
+    };
 
 		checkTime = function () { // Check if the time has been updated
-			var element = $(lastTimeSelector),
-				time = element.text(),
-				timeParts = time.split(timeDelimiter),
-				then = new Date(),
-				now = new Date(),
-				timeDifference = 0,
-				timestamp = now.getTime(),
-				realSeconds = null,
-				sinceLastUpdate = null,
-				//isGodUpdate = $(godInteractionSelector, element).length > 0,
-				timeText;
+      if (!(lastUpdate && timeAtUpdate)) {
+        updateTime();
+        currentSecond = null;
+      } else {
+        var now = Date.now(),
+          difference = timeAtUpdate && lastUpdate ? now - lastUpdate : 0;
 
-			// Update the current second
-			currentSecond = now.getSeconds();
-			realSeconds = currentSecond;
+        // Check if the time needs to be updated
+        if (!lastUpdate || difference >= maxTimeDifference) {
+          updateTime();
+        }
 
-			// Get the actual date of the time
-			timeParts = time.split(timeDelimiter);
-			then.setHours(parseInt(timeParts[0], 10));
-			then.setMinutes(parseInt(timeParts[1], 10));
+        // Update display
+        currentSecond = (new Date((timeAtUpdate || now) + difference)).getSeconds();
+      }
 
-			// Determine the minute difference between the displayed time and the local time
-			timeDifference = Math.floor(((timestamp % halfDay) - (then.getTime() % halfDay)) / millisecondsPerMinute);
-
-			if (timeDifference !== 0) {
-				realSeconds += timeDifference * secondsPerMinute;
-			}
-
-			// Determine the time difference since the last update
-			if (lastUpdate) {
-				sinceLastUpdate = Math.abs(timestamp - lastUpdate);
-			}
-
-			// Update this as the latest update
-			lastUpdate = timestamp;
-
-			// Determine if the time is new
-			if (time !== lastTime) { //&& !isGodUpdate) {
-				// Determine whether the current time can be considered valid
-				if (sinceLastUpdate !== null && sinceLastUpdate < maxTimeDifference) {
-					// Make the time readable
-					timeText = (pad(now.getHours() % 12) || 12) + timeDelimiter + pad(now.getMinutes()) + timeDelimiter + pad(currentSecond);
-
-					// Add the time to the collection
-					addTime(realSeconds, timeText);
-				}
-
-				// Update last time
-				lastTime = time;
-			}
-
-			// Update display
-			updateDisplay();
+      updateDisplay();
 		};
-
-		// Initiate the display
-		if (allSeconds.length) {
-			updateDisplay();
-		}
 
 		// Add queue actions
 		queueButton.click(function () { updateQueue(true); });
@@ -288,7 +151,7 @@
 
 		// Add styles
 		$("head").append("<style id=godville-timer-stylesheet>\n" +
-			"#godville-timer { position: fixed; left: 0; bottom: 32px; padding: 8px 16px; width: 100%; background: rgba(0,0,0,0.5); color: white; font-size: 150%; text-align: center; pointer-events: none; overflow: hidden; z-index: 9999; box-sizing: border-box; }" +
+			"#godville-timer { position: fixed; left: 0; bottom: 32px; padding: 8px 16px; width: 100%; background: rgba(0,0,0,0.5); color: white; font-size: 150%; text-align: right; pointer-events: none; overflow: hidden; z-index: 9999; box-sizing: border-box; }" +
 			"#godville-timer > div { display: inline-block; padding: 0 8px; height: 64px; line-height: 64px; font-size: 200%; background: rgba(0,0,0,0.1) }" +
 			"#godville-timer > div + div { margin-left: 10px }" +
 			"#godville-timer > #current-second { background: rgba(0,0,0,0.3) }" +
@@ -297,8 +160,7 @@
 			"#godville-timer > #current-second.before { color: #f7f7b7 }" +
 			"#godville-timer > #current-second.during { color: #a1d1a1 }" +
 			"#godville-timer > #current-second.exact { color: #3ada3a }" +
-			"#godville-timer .queue { display: none; position: absolute; left: 0; top: 8px; background: rgba(0,0,0,0.5); font-size: 75%; text-align: center; line-height: 250%; border-radius: 4px }" +
-			"#godville-timer.is-ready .queue { display: block }" +
+			"#godville-timer .queue { position: absolute; left: 0; top: 8px; background: rgba(0,0,0,0.5); font-size: 75%; text-align: center; line-height: 250%; border-radius: 4px }" +
 			"#godville-timer .queue .queued, #godville-timer .queue.active .not-queued { display: none }" +
 			"#godville-timer .queue.active .queued, #godville-timer .queue span { display: block }" +
 			"#godville-timer .queue button { pointer-events: all }" +
